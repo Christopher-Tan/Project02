bf2194f72df4bf94cd5e8ee996cd95b2ad3df8f7
theofahey <86529092+theofahey@users.noreply.github.com>
2022-01-23 18:52:33 -0500

    Update render.h

--- render.h
+++ render.h
@@ -8,6 +8,7 @@ void draw_circle(SDL_Renderer * renderer, int centreX, int centreY, int radius, 
 void render_o(SDL_Renderer * renderer, int box, int row, int column, const SDL_Color * color);
 void render_board(SDL_Renderer * renderer, const char board[9][9], const SDL_Color *player_x_color, const SDL_Color *player_o_color);
 void render_running_state(SDL_Renderer * renderer, struct game_t *game);
+void render_lose(SDL_Renderer * renderer, struct game_t * game);
 void render_game_over(SDL_Renderer * renderer, struct game_t * game, const SDL_Color * color);
 void rendering_game(SDL_Renderer * renderer, struct game_t *game);
 #endif
